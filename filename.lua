f16c315e9f8534a1daa83020fa5ac4c56b4f1c40
RQHQKILLER <swep66011@gmail.com>
2024-10-18 13:06:12 +0300

    Update filename.lua

--- filename.lua
+++ filename.lua
@@ -1,1 +1,19 @@
-Print("Hello World!")
+getgenv().Camlock_Settings = {
+        Prediction = 0.157,
+        AimPart = "Torso",
+        Key = "z",
+        AutoPrediction = false,
+        Notification = true,
+        Button = true,
+        AntiGroundShots = false,
+        UnderGroundResolver = false,
+
+        -- DO NOT TOUCH THIS OR THE CAMLOCK WILL NOT WORK --
+        Version = "2.5.1",
+        Credits = "space_0999",
+        DiscordServer = "discord.gg/SKhamGzTdn"
+    }
+    
+    
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/elxocasXD/Trip-Hub/main/Scripts/Cam%20Lock.lua'))() 
+
