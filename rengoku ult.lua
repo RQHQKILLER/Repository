f83b0c80409771f474e4c1f91f8899dfbd292443
RQHQKILLER <swep66011@gmail.com>
2025-02-28 22:44:32 +0300

    Update rengoku ult.lua

--- rengoku ult.lua
+++ rengoku ult.lua
@@ -1,1 +1,335 @@
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local dragonHead = rengoku:WaitForChild("DragonHead")
 
+-- البحث عن HeadToCharacter داخل الموديل DragonHead وتعطيل الخاصية Enabled
+local headToCharacter = dragonHead:FindFirstChild("HeadToCharacter")
+
+if headToCharacter then
+    headToCharacter.Enabled = false
+    print("تم تعطيل الخاصية Enabled داخل HeadToCharacter.")
+end
+
+---------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local rengokuCharge = rengoku:WaitForChild("RengokuCharge")
+
+-- حذف الكائن RengokuCharge إذا كان جزء (Part)
+if rengokuCharge and rengokuCharge:IsA("Part") then
+    rengokuCharge:Destroy()
+    print("تم حذف الكائن RengokuCharge.")
+end
+
+----------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("White")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+--------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("Orange")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+----------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("LightOrange")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+----------------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("Front")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("White")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+--------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("Orange")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+----------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("LightOrange")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-----------------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("StripeYellow")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+----------------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local aura = rengoku:WaitForChild("Aura")
+local white = aura:WaitForChild("Front")
+
+-- تعيين قيمة السطوع Brightness إلى 100
+if white then
+    white.Brightness = 1000
+    print("تم تعيين قيمة السطوع Brightness للكائن White إلى 100.")
+end
+
+-------------
+----------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local explosion = rengoku:WaitForChild("Explosion")
+
+-- تغيير الـ SoundId
+if explosion:IsA("Sound") then
+    explosion.SoundId = "rbxassetid://8936215056"
+    print("تم تغيير الـ SoundId للصوت Explosion إلى rbxassetid://8936215056.")
+end
+
+-------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local ost = rengoku:WaitForChild("OST")
+
+-- تغيير الـ SoundId وتعيين TimePosition إلى 0
+if ost:IsA("Sound") then
+    ost.SoundId = "rbxassetid://1838993667"
+    ost.TimePosition = 0
+    print("تم تغيير الـ SoundId للصوت Ost وتعيين TimePosition إلى 0.")
+end
+
+----------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("UnknowingFire")
+local explosion = rengoku:WaitForChild("DashStart")
+
+-- تغيير الـ SoundId
+if explosion:IsA("Sound") then
+    explosion.SoundId = "rbxassetid://1839296012"
+    print("تم تغيير الـ SoundId للصوت Explosion إلى rbxassetid://8936215056.")
+end
+
+----------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local explosion = rengoku:WaitForChild("Explosion")
+
+-- تغيير الـ SoundId
+if explosion:IsA("Sound") then
+    explosion.SoundId = "rbxassetid://8936215056"
+    print("تم تغيير الـ SoundId للصوت Explosion إلى rbxassetid://8936215056.")
+end
+
+-------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("Rengoku")
+local ost = rengoku:WaitForChild("OST")
+
+-- تغيير الـ SoundId وتعيين TimePosition إلى 0
+if ost:IsA("Sound") then
+    ost.SoundId = "rbxassetid://1838993667"
+    ost.TimePosition = 0
+    print("تم تغيير الـ SoundId للصوت Ost وتعيين TimePosition إلى 0.")
+end
+
+----------------------
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local sounds = ReplicatedStorage:WaitForChild("Sounds")
+local fireBreathing = sounds:WaitForChild("FireBreathing")
+local rengoku = fireBreathing:WaitForChild("UnknowingFire")
+local explosion = rengoku:WaitForChild("DashStart")
+
+-- تغيير الـ SoundId
+if explosion:IsA("Sound") then
+    explosion.SoundId = "rbxassetid://1839296012"
+    print("تم تغيير الـ SoundId للصوت Explosion إلى rbxassetid://8936215056.")
+end
+
+--------------
+
+local ReplicatedStorage = game:GetService("ReplicatedStorage")
+local utils = ReplicatedStorage:WaitForChild("Utils")
+local fireBreathing = utils:WaitForChild("FireBreathing")
+local vanishingDust = fireBreathing:WaitForChild("VanishingDust")
+local middle = vanishingDust:WaitForChild("Middle")
+local dust = middle:WaitForChild("Dust2")
+-- حذف الكائن Dust
+if dust then
+    dust:Destroy()
+    print("تم حذف الكائن Dust.")
+else
+    print("لم يتم العثور على الكائن Dust.")
+end
+
+-------------------
+local replicatedStorage = game:GetService("ReplicatedStorage")
+local animationsFolder = replicatedStorage:WaitForChild("Animations")
+local beastBreathingFolder = animationsFolder:WaitForChild("Firebreathing")
+local ExplosiveRushRun1 = beastBreathingFolder:WaitForChild("Rengoku")
+local ExplosiveRushRun = ExplosiveRushRun1:WaitForChild("Hold")
+
+if ExplosiveRushRun:IsA("Animation") then
+    ExplosiveRushRun.AnimationId = "rbxassetid://17397193556"
+end
